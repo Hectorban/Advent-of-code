d75ba470e6939ab07be7671a2390b57016b841b3
Héctor Banderas <hectorban1234@gmail.com>
2022-11-23 04:20:56 -0600

    feat: first of 2k17

---
+++ aoc-2017/src/bin/1.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("inputs/1-2.txt").expect("Problem while reading from file.");
+    let mut chars = input.chars();
+    chars.next_back();
+    let values: Vec<u32> = chars.map(|x| x.to_digit(10).unwrap()).collect();
+
+    let mut sum = 0;
+    let mut last_value = 0;
+    for val in &values {
+        if val == &last_value {
+            sum += val
+        }
+        last_value = *val
+    }
+    if values.last().unwrap() == values.first().unwrap() {
+        sum += values.first().unwrap();
+    }
+
+    println!("Sum: {}", sum);
+}
+
