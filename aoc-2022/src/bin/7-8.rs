023bd6655d2d7f81653a0d969b771c2651c2c4a6
Héctor Banderas <hectorban1234@gmail.com>
2022-12-10 03:34:42 -0600

    feat: FINALLY day 4

--- aoc-2022/src/bin/7-8.rs
+++ aoc-2022/src/bin/7-8.rs
@@ -1,4 +1,4 @@
-use std::{fs, ops::Range};
+use std::{fs, collections::HashSet};
 
 fn main() {
     let input = fs::read_to_string("inputs/7-8.txt").expect("Problem while reading from file.");
@@ -6,40 +6,30 @@ fn main() {
     println!("Second: {}", second_part(input));
 }
 
-fn first_part(input: &String) -> i32 {
+fn first_part(input: &String) -> usize {
     let lines = input.lines();
-    let result = lines.into_iter().map(|line| {
+    lines.into_iter().filter(|line| {
         let mut split = line.split(",");
         let first_elf = gen_range(split.next().unwrap());
         let second_elf = gen_range(split.next().unwrap());
-        let second_to_first = first_elf.clone().into_iter().all(|item| second_elf.contains(&item));
-        let first_to_second = second_elf.clone().into_iter().all(|item| first_elf.contains(&item));
-        if first_to_second {
-            println!("{:?} contains {:?}", first_elf, second_elf);
-        }
-        if second_to_first {
-            println!("{:?} contains {:?}", second_elf, first_elf);
-        }
-        first_to_second | second_to_first
-    }).fold(0, |acc, x| {
-        if x {
-            acc + 1
-        } else {
-            acc + 0
-        }
-    });
-    println!("{}", result);
-    0
+        (&first_elf - &second_elf).is_empty() || (&second_elf - &first_elf).is_empty()
+    }).collect::<Vec<&str>>().len()
 }
 
-fn gen_range(range: &str) -> Range<i32> {
+fn gen_range(range: &str) -> HashSet<i32> {
     let mut split = range.split("-");
     let start: i32 = split.next().unwrap().parse().unwrap();
     let end: i32 = split.next().unwrap().parse().unwrap();
-    start..end
+    std::ops::RangeInclusive::new(start, end).collect()
 }
 
-fn second_part(input: String) -> i32 {
-    0
+fn second_part(input: String) -> usize {
+    let lines = input.lines();
+    lines.into_iter().filter(|line| {
+        let mut split = line.split(",");
+        let first_elf = gen_range(split.next().unwrap());
+        let second_elf = gen_range(split.next().unwrap());
+        !first_elf.intersection(&second_elf).collect::<Vec<&i32>>().is_empty()
+    }).collect::<Vec<&str>>().len()
 }
     
